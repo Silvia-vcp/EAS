82b2eff21bd0690fae5e22fa64ab268c87e34685
Silvia-vcp <57897967+Silvia-vcp@users.noreply.github.com>
2023-04-21 09:30:45 +0200

    Add files via upload

---
+++ 23E03-master/principal/RaEvent.java
@@ -0,0 +1,31 @@
+package raf.principal;
+
+
+
+//TITULO
+//Esta clase es un objeto que es capaz de generar eventos
+public class RaEvent extends java.util.EventObject
+{
+    
+    public static final int DISPATCH_REQUEST = 1;
+    
+    
+    public static final int SLEEP_REQUEST = 2;
+    
+   
+    public static final int DESTROY_REQUEST = 3;
+    
+  
+    protected int id;
+    
+   
+    public RaEvent(Object obj, int id){
+        super(obj); 
+        this.id = id;
+    } 
+    
+    
+    public int getID(){
+        return id;
+    }    
+}    
